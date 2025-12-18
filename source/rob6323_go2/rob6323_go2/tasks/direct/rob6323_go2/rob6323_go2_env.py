b7fb4c4d17387df345b8e3d602c22590043402db
vedAnts256 <vedant.ghatnekar@gmail.com>
2025-12-18 18:32:45 -0500

    Bug Fix 1

--- source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env.py
+++ source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env.py
@@ -450,8 +450,8 @@ class Rob6323Go2Env(DirectRLEnv):
         swing_mask[:2] = 1
         
         clearance_error = torch.zeros(4)
-        clearance_error[:2] = front_target - front_foot_heights
-        clearance_error[2:] = back_target - back_foot_heights
+        clearance_error[:,:2] = front_target - front_foot_heights
+        clearance_error[:,2:] = back_target - back_foot_heights
         clearance_error = torch.clamp(clearance_error, min=0.0)
         clearance_error = clearance_error * swing_mask.float()
         
@@ -466,7 +466,7 @@ class Rob6323Go2Env(DirectRLEnv):
         
         force_threshold = 1.0
         contact_indicator = torch.clamp(force_magnitude / force_threshold, max=1.0)
-        contact_indicator[:2] = 0.0
+        contact_indicator[:,:2] = 0.0
         
         # Reward when desired_contact_states is high and feet are in contact
         reward = torch.sum(self.desired_contact_states * contact_indicator, dim=1)
